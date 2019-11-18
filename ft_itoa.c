60e4a44bf10888242846b38cc4416a0b42001ce9
Diego alberto Lobos matus de la parra <dlobos-m@c3r14s6.42madrid.com>
2019-11-18 16:15:50 +0100

    meto ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlobos-m <dlobos-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/15 19:04:38 by dlobos-m          #+#    #+#             */
+/*   Updated: 2019/11/18 14:17:36 by dlobos-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		numcar(long int n)
+{
+	long int i;
+
+	i = 0;
+	if (n == 0)
+	{
+		i = 1;
+		return (i);
+	}
+	if (n < 0)
+	{
+		n *= -1;
+		i++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	long int	i;
+	char		*pnt;
+	long int	num;
+
+	num = (long int)n;
+	i = numcar(num);
+	if (!(pnt = malloc((i + 1) * sizeof(char))))
+		return (0);
+	pnt[i] = '\0';
+	pnt[0] = '0';
+	if (num < 0)
+		num *= -1;
+	while (i-- >= 0)
+	{
+		pnt[i] = (num % 10) + '0';
+		num = num / 10;
+	}
+	if (n < 0)
+		pnt[0] = '-';
+	return (pnt);
+}
