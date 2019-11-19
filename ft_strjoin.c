8396b4c6758db158278b3fce6f521e2e2d368b25
Diego alberto Lobos matus de la parra <dlobos-m@c3r12s5.42madrid.com>
2019-11-19 20:00:47 +0100

    version buena strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dlobos-m <dlobos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 14:18:06 by dlobos-m          #+#    #+#             */
-/*   Updated: 2019/11/15 13:55:35 by dlobos-m         ###   ########.fr       */
+/*   Updated: 2019/11/19 20:00:13 by dlobos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*pnt;
 	int		i;
 	int		j;
+	int		x;
 
-	i = 0;
-	j = 0;
 	if (!(s1 && s2))
 		return (NULL);
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	pnt = (char*)malloc((i + j) * sizeof(char));
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	pnt = (char*)malloc((i + j + 1) * sizeof(*pnt));
 	if (pnt == NULL)
 		return (NULL);
-	while (j-- >= 0)
-		pnt[i + j] = s2[j];
-	while (i-- >= 0)
-		pnt[i] = s1[i];
+	x = 0;
+	while (*s1)
+	{
+		pnt[x] = *s1++;
+		x++;
+	}
+	while (*s2)
+	{
+		pnt[x] = *s2++;
+		x++;
+	}
+	pnt[x] = '\0';
 	return (pnt);
 }
