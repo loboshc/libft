e07990a731f6cc2becd22de5b7923a568973584a
Diego alberto Lobos matus de la parra <dlobos-m@c3r11s6.42madrid.com>
2019-11-19 14:17:58 +0100

    corregido errores Libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dlobos-m <dlobos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 16:21:24 by dlobos-m          #+#    #+#             */
-/*   Updated: 2019/11/15 13:55:30 by dlobos-m         ###   ########.fr       */
+/*   Updated: 2019/11/19 14:16:44 by dlobos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strdup(const char *s)
 	pnt = malloc(i * sizeof(char));
 	if (pnt == NULL)
 		return (NULL);
-	while (i-- >= 0)
+	while (i-- > 0)
 		pnt[i] = s[i];
 	return (pnt);
 }
