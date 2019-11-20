117073273d0bc1ff679c8950acd00a0d2cf7626a
Diego alberto Lobos matus de la parra <dlobos-m@c3r12s6.42madrid.com>
2019-11-20 18:14:13 +0100

    solucionando errores

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dlobos-m <dlobos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 17:11:01 by dlobos-m          #+#    #+#             */
-/*   Updated: 2019/11/15 13:55:07 by dlobos-m         ###   ########.fr       */
+/*   Updated: 2019/11/20 18:03:07 by dlobos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	cad = (unsigned char*)s1;
 	cad1 = (unsigned char*)s2;
 	i = 0;
-	while (cad[i] == cad1[i] && ++i < n)
-		;
+	while (i < n && cad[i] == cad1[i])
+		i++;
 	if (i == n)
 		return (0);
+	if (cad[i] != cad1[i])
+		return (cad[i] - cad1[i]);
 	else
-		return ((int)cad[i] - cad1[i]);
+		return (0);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dlobos-m <dlobos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 16:20:16 by dlobos-m          #+#    #+#             */
-/*   Updated: 2019/11/15 13:55:13 by dlobos-m         ###   ########.fr       */
+/*   Updated: 2019/11/20 17:53:42 by dlobos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 void	*ft_memcpy(void *str1, const void *str2, size_t n)
 {
-	size_t				i;
 	unsigned char		*cad1;
 	unsigned char		*cad2;
 
 	cad1 = (unsigned char*)str1;
 	cad2 = (unsigned char*)str2;
-	i = 0;
-	while (i < n && (cad1 != NULL || cad2 != NULL))
-	{
-		cad1[i] = cad2[i];
-		i++;
-	}
-	return (cad1);
+	if (!n || str1 == str2)
+		return (str1);
+	while (n--)
+		*cad1++ = *cad2++;
+	return (str1);
 }
